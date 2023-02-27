9a586076baf3cf9486f0d847d881fb4bde62296b
vpalma78-01@yahoo.fr <117671625+RonickP@users.noreply.github.com>
2023-02-27 17:56:56 +0100

    fed

---
+++ screens/AlimentExcluScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AlimentExcluScreen() {
+  return (
+    <View>
+      <Text>AlimentExcluScreen</Text>
+    </View>
+  )
+}
---
+++ screens/BienvenueScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function BienvenueScreen() {
+  return (
+    <View>
+      <Text>BienvenueScreen</Text>
+    </View>
+  )
+}
