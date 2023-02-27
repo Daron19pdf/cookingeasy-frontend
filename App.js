06e39387e1b5fd4d78e0433144994d6df6b63a67
Massine <fmassiine@gmail.com>
2023-02-27 17:45:38 +0100

    foyer_Massine

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import * as Progress from 'react-native-progress';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useState } from 'react';
 
 export default function App() {
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <Text style={styles.title}>Mon Foyer</Text>
+      <Text>Nombre de personnes pour mes recettes</Text>
+      <Text style={styles.Number}>{count1}</Text>
+      <View style={styles.Button}>
+      <Button style={styles.buttonMax}
+          title=" + "
+          color={'#FA8C8E'}
+        onPress={() => setCount1(count1 + 1)}
+      />
+      <Button style={styles.buttonMin}
+          title=" - "
+          color={'#FA8C8E'}
+        onPress={() => (count1 > 0 && setCount1(count1 - 1))}
+      />
+      </View>
+      <Text>Nombre de recettes pour la semaine</Text>
+      <Text style={styles.Number}>{count2}</Text>
+      <View style={styles.Button}>
+      <Button style={styles.buttonMax}
+          title="+"
+          color={'#FA8C8E'}
+        onPress={() => setCount2(count2 + 1)}
+      />
+      <Button style={styles.buttonMin}
+          title="-"
+          color={'#FA8C8E'}
+        onPress={() => (count2 > 0 &&setCount2(count2 - 1))}
+      />
+      </View>
+        <View style={styles.botomButon}>
+        <TouchableOpacity style={styles.previous} onPress={() => navigation.navigate('InfoScreen')}>
+        <FontAwesome name='arrow-left' size={15} />
+        </TouchableOpacity>
+        <Button style={styles.suivant}
+            title="Suivant"
+            color={'#FA8C8E'}
+        onPress={() => navigation.navigate('EquipementScreen')}
+      />
+        </View>
+         <Progress.Bar width={250} borderWidth={1} progress={0.5} height={15} color={'#FA8C8E'} indeterminateAnimationDuration={2000} />
+    </KeyboardAvoidingView>
   );
 }
 
@@ -15,6 +66,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+
+  title: {
+  display: 'flex',
+  width: '80%',
+  fontSize: 35,
+  fontWeight: '600',
+  justifyContent: 'center',
+  marginTop: 50,
+  },
+  Number: {
+    fontSize: 30,
+  },
+  Button: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    width: 100,
+  },
+  buttonMax: {
+    width: 100,
+    marginRight: 20,
+  },
+  buttonMin: {
+    width: 150,
+    marginLeft: 20,
+
+  },
+  botomButon: {
+    flexDirection: 'row',
+    alignItems: 'space-around',
+    marginRight: 20,
+    marginLeft: 20,
   },
 });
