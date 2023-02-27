32337055f1ca6d516f10aae94e06d7ed18445ec4
Cyrille <daron19pdf@gmail.com>
2023-02-27 17:51:15 +0100

    cyrille-27/02

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IngredientScreen from './screens/AlimentExcluScreen';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import ingredient from './reducers/ingredient';
+
+
+const Stack = createNativeStackNavigator();
+
+const store = configureStore({
+  reducer: {ingredient},
+ });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer style={styles.container}>
+       <Stack.Navigator screenOptions={{ headerShown: false }}>
+       <Stack.Screen name="Exclu" component={IngredientScreen} />
+       </Stack.Navigator>
+       </NavigationContainer>
+      </Provider>
   );
 }
 
