23404ea2295ae5962de4e2a4992190f9d7c9da3a
vpalma78-01@yahoo.fr <117671625+RonickP@users.noreply.github.com>
2023-03-06 09:40:37 +0100

    initlundimatin

--- screens/BienvenueScreen.js
+++ screens/BienvenueScreen.js
@@ -7,20 +7,25 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
+} from "react-native";
 
-
-export default function BienvenueScreen ({ navigation }) {
+export default function BienvenueScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bienvenue</Text>
-        <Image style={styles.image} source={require('../assets/Bienvenue.jpg')} />
-        <TouchableOpacity style={styles.button1} onPress={() => navigation.navigate('ConnectionScreen')}>
-          <Text style={styles.text}>Se connecter</Text>
-          </TouchableOpacity>
-        <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('InscriptionScreen')}>
-          <Text style={styles.text2}>S'inscrire</Text>
-          </TouchableOpacity>
+      <Image style={styles.image} source={require("../assets/Bienvenue.jpg")} />
+      <TouchableOpacity
+        style={styles.button1}
+        onPress={() => navigation.navigate("ConnectionScreen")}
+      >
+        <Text style={styles.text}>Se connecter</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button2}
+        onPress={() => navigation.navigate("InscriptionScreen")}
+      >
+        <Text style={styles.text2}>S'inscrire</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -28,64 +33,64 @@ export default function BienvenueScreen ({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
-background: {
-  width: '100%',
-  height: '100%',
-  backgroundColor: 'white'
-},
-image: {
-    width: '100%',
-    height: '50%',
+  background: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "100%",
+    height: "50%",
   },
   title: {
     fontSize: 35,
-    fontWeight: '600',
-    fontFamily: 'Helvetica',
+    fontWeight: "600",
+    fontFamily: "bold",
     marginBottom: 40,
   },
   button1: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: '#E3C7F9',
-    shadowColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: "#E3C7F9",
+    shadowColor: "rgba(0, 0, 0, 0.1)",
     shadowOpacity: 0.8,
-    shadowRadius: 5 ,
-    shadowOffset : { width: 1, height: 13}
+    shadowRadius: 5,
+    shadowOffset: { width: 1, height: 13 },
   },
   button2: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginTop: 15,
-    shadowColor: 'rgba(0, 0, 0, 0.1)',
+    shadowColor: "rgba(0, 0, 0, 0.1)",
     shadowOpacity: 0.8,
-    shadowRadius: 5 ,
-    shadowOffset : { width: 1, height: 13}
+    shadowRadius: 5,
+    shadowOffset: { width: 1, height: 13 },
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
   text2: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: '#FA8C8E',
+    color: "#f4511e",
   },
 });
--- screens/index.js
+++ screens/index.js
@@ -1,93 +1,144 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    TouchableOpacity,
-  } from 'react-native';
-  
-  
-  export default function Index ({ navigation}) {
-    return (
-        <View style={styles.container}>
-        <ScrollView style={styles.menu}>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('HomeScreen')}>
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+export default function Index({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.menu}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("HomeScreen")}
+        >
           <Text style={styles.buttonText}>Home screen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('AlimentExcluScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("AlimentExcluScreen")}
+        >
           <Text style={styles.buttonText}>AlimentExcluScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('ConnectionScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("ConnectionScreen")}
+        >
           <Text style={styles.buttonText}>ConnectionScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('FoyerScreen')}>
-          <Text style={styles.buttonText}>Foyer  Screen</Text>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("FoyerScreen")}
+        >
+          <Text style={styles.buttonText}>Foyer Screen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('EquipementScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("EquipementScreen")}
+        >
           <Text style={styles.buttonText}>Equipement Screen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('BienvenueScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("BienvenueScreen")}
+        >
           <Text style={styles.buttonText}>Bienvenue Screen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('RegimeScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("RegimeScreen")}
+        >
           <Text style={styles.buttonText}>RegimeScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('ConservationScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("ConservationScreen")}
+        >
           <Text style={styles.buttonText}>ConservationScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('FavorisScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("FavorisScreen")}
+        >
           <Text style={styles.buttonText}>FavorisScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('FelicitationsScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("FelicitationsScreen")}
+        >
           <Text style={styles.buttonText}>FelicitationsScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('InfoScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("InfoScreen")}
+        >
           <Text style={styles.buttonText}>InfoScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('InscriptionScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("InscriptionScreen")}
+        >
           <Text style={styles.buttonText}>InscriptionScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('MenuScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("MenuScreen")}
+        >
           <Text style={styles.buttonText}>MenuScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('MenuScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("MenuScreen")}
+        >
           <Text style={styles.buttonText}>MenuScreen2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('NewRecetteScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("NewRecetteScreen")}
+        >
           <Text style={styles.buttonText}>NewRecetteScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('PrefSemaineScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("PrefSemaineScreen")}
+        >
           <Text style={styles.buttonText}>PrefSemaineScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('ProfilScreen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("ProfilScreen")}
+        >
           <Text style={styles.buttonText}>ProfilScreen</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.next} onPress={() => navigation.navigate('CuisineEtape1Screen')}>
+        <TouchableOpacity
+          style={styles.next}
+          onPress={() => navigation.navigate("CuisineEtape1Screen")}
+        >
           <Text style={styles.buttonText}>CuisineEtape1Screen</Text>
         </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
 
-        </ScrollView>
-        </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    menu: {
-        marginTop: 70,
-        },
-    next: {
-        flexDirection: 'row',
-        backgroundColor: '#FA8C8E',
-        margin: 10,
-        width: 200,
-        height: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-        },
-
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  menu: {
+    marginTop: 70,
+  },
+  next: {
+    flexDirection: "row",
+    backgroundColor: "#f4511e",
+    margin: 10,
+    width: 200,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
