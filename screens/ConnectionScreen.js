17794c146ed29ba1f71b53685294fd53ab9414a4
Massine <fmassiine@gmail.com>
2023-03-09 09:52:15 +0100

    Info-Screen Massine

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -28,7 +28,7 @@ export default function ConnectionScreen({ navigation }) {
   };
 
   const handleConnection = () => {
-    fetch(`${BACKEND_ADDRESS}/user/signin`, {
+    fetch(`${BACKEND_ADDRESS}user/signin`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -38,7 +38,7 @@ export default function ConnectionScreen({ navigation }) {
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data) {
+        if (data.result) {
           dispatch(
             login({
               pseudo: pseudo,
